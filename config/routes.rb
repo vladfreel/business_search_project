f63ef9d0eb4cd560939b134cf11a707649424036
vladfreel <vladfreelmaster@gmail.com>
2020-08-03 11:37:53 +0300

    Add devise for auth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
+  devise_scope :user do
+    authenticated :user do
+      root to: 'books#index'
+    end
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
